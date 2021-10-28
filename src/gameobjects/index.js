8e4091656e2edbc40a1234f66782799af3c1d431
Maarten Menten <>
2021-10-28 17:00:54 +0200

    Cleanup.

--- src/gameobjects/index.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = 
-{
-	Sprite  : require( './Sprite' ),
-	Agent   : require( './Agent' ),
-	Weapon  : require( './Weapon' ),
-	Firearm : require( './Firearm' ),
-	Bullet  : require( './Bullet' ),
-};
